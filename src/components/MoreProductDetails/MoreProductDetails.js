6e4001f27a32ac34b095f07aa6ad5de9a17e1225
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-06-11 22:36:30 +0600

    product

--- src/components/MoreProductDetails/MoreProductDetails.js
+++ src/components/MoreProductDetails/MoreProductDetails.js
@@ -14,16 +14,25 @@ const MoreProductDetails = () => {
   }, []);
   const details = products.find((s) => s.id === id);
   return (
-    <div className=" justify-items-center py-6  ">
-      <div className="details gap-0  grid sm:grid-cols-1 lg:grid-cols-1 justify-items-center items-center mx-5  ">
-        <div className=" ">
+    <div className=" flex items-center justify-center  py-6  ">
+
+
+
+
+ 
+      <div className="details gap-0  grid sm:grid-cols-1     grid-cols-1  place-items-center  mx-5  ">
+        <div className="   ">
           <img
             src={details?.img}
-            className="  py-5 p-8   object-cover    w-3/4 mx-auto"
+            className="  py-5 p-8    object-cover    w-3/4 mx-auto"
             alt=""
           />
         </div>
-        <div className=" ml-5">
+
+
+
+        <div className="w-3/4 ml-9">
+     
           <div className="py-5 p-5    text-white  ">
             <h3 className="font-bold py-2" style={{ fontSize: "25px " }}>
               {" "}
@@ -32,86 +41,86 @@ const MoreProductDetails = () => {
 
 
 
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
-              <h1 className="" style={{ fontSize: "20px" }}>
+            <div className="flex border  items-center gap-2 ">
+           
+              <h1 className=" " style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationsone}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
+            <div className="flex  border items-center gap-2 ">
+           
               <h1 className="" style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationstwo}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
-              <h1 className="" style={{ fontSize: "20px" }}>
+            <div className="flex border  items-center gap-2 ">
+           
+              <h1 className=" " style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationsthree}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
+            <div className="flex border  items-center gap-2 ">
+           
               <h1 className="" style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationsfour}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
+            <div className="flex border  items-center gap-2 ">
+           
               <h1 className="" style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationsfive}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
+            <div className="flex border  items-center gap-2 ">
+           
               <h1 className="" style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationssix}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
+            <div className="flex border  items-center gap-2 ">
+           
               <h1 className="" style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationsseven}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
+            <div className="flex  border items-center gap-2 ">
+           
               <h1 className="" style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationseight}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
+            <div className="flex border  items-center gap-2 ">
+           
               <h1 className="" style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationsnine}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
+            <div className="flex  border items-center gap-2 ">
+           
               <h1 className="" style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationsten}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
-              <h1 className="" style={{ fontSize: "20px" }}>
+            <div className="flex border   items-center gap-2 ">
+           
+              <h1 className=" " style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationseleven}
               </h1>
             </div>
-            <div className="flex   items-center gap-2 ">
-              <BiRightArrow className="text-2xl"></BiRightArrow>
-              <h1 className="" style={{ fontSize: "20px" }}>
+            <div className="flex border  items-center gap-2 ">
+           
+              <h1 className=" " style={{ fontSize: "20px" }}>
                 {" "}
                 {details?.description.specificationsthirten}
               </h1>
